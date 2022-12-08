1fad9fa7177b39ac79ae9c474858ac9672451b50
ZaryabAlam <zaryabalam97@gmail.com>
2022-12-08 21:17:37 +0500

    Project Progress

--- lib/main.dart
+++ lib/main.dart
@@ -127,14 +127,60 @@ class _MyAppState extends State<MyApp> {
 /////////////////////////// Permission //////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////
   Widget _buildPermissionSheet() {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () {
-            Permission.locationWhenInUse.request().then((value) {
-              _fetchPermissionStatus();
-            });
-          },
-          child: const Text("Request Permission")),
+    final _h = MediaQuery.of(context).size.height;
+    final _w = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Container(
+          color: Color(0XFFf0f1ec),
+        ),
+        Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: _h * 0.2,
+              ),
+              Container(
+                height: 230,
+                width: 230,
+                decoration: BoxDecoration(
+                    image: DecorationImage(image: AssetImage("comp-logo.png"))),
+              ),
+              Container(
+                height: _h * 0.07,
+                width: _w * 0.6,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Color(0xFF21313f)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ))),
+                    onPressed: () {
+                      Permission.locationWhenInUse.request().then((value) {
+                        _fetchPermissionStatus();
+                      });
+                    },
+                    child: const Text("Lets go!")),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: _h * 0.3,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("comp-bottom.png"), fit: BoxFit.fill)),
+            ),
+          ],
+        )
+      ],
     );
   }
 }
