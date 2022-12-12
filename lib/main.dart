67c21ce9178d41b63b3d932866b1d3e1da8cbe59
ZaryabAlam <zaryabalam97@gmail.com>
2022-12-12 23:08:46 +0500

    Project Close

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,18 @@
-import 'package:compass/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_advanced_drawer/flutter_advanced_drawer.dart';
+import 'package:flutter_compass/flutter_compass.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'dart:math' as math;
+import 'dart:ui';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: HomeScreen(),
-    );
+    return HomeScreen();
   }
 }
 
@@ -22,6 +23,23 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final _advancedDrawerController = AdvancedDrawerController();
+  bool _hasPermissions = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPermissionStatus();
+  }
+
+  void _fetchPermissionStatus() {
+    Permission.locationWhenInUse.status.then((status) {
+      if (mounted) {
+        setState(() {
+          _hasPermissions = (status == PermissionStatus.granted);
+        });
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +72,13 @@ class _HomeScreenState extends State<HomeScreen> {
             ),
           ),
         ),
-        body: Home(),
+        body: Builder(builder: (context) {
+          if (_hasPermissions) {
+            return _buildCompass();
+          } else {
+            return _buildPermissionSheet();
+          }
+        }),
       ),
       drawer: SafeArea(
         child: Container(
@@ -65,11 +89,11 @@ class _HomeScreenState extends State<HomeScreen> {
               mainAxisSize: MainAxisSize.max,
               children: [
                 Container(
-                  width: 128.0,
-                  height: 128.0,
+                  width: 125.0,
+                  height: 125.0,
                   margin: const EdgeInsets.only(
-                    top: 24.0,
-                    bottom: 64.0,
+                    top: 15.0,
+                    bottom: 35.0,
                   ),
                   clipBehavior: Clip.antiAlias,
                   decoration: BoxDecoration(
@@ -113,7 +137,7 @@ class _HomeScreenState extends State<HomeScreen> {
                     Text(
                       "Scan for more!",
                       style: TextStyle(
-                          fontSize: 20,
+                          fontSize: 18,
                           color: Colors.white,
                           fontWeight: FontWeight.bold),
                     ),
@@ -121,12 +145,12 @@ class _HomeScreenState extends State<HomeScreen> {
                 ),
                 DefaultTextStyle(
                   style: TextStyle(
-                    fontSize: 12,
+                    fontSize: 10,
                     color: Colors.white54,
                   ),
                   child: Container(
                     margin: const EdgeInsets.symmetric(
-                      vertical: 16.0,
+                      vertical: 6.0,
                     ),
                     child: Column(
                       children: [
@@ -148,5 +172,143 @@ class _HomeScreenState extends State<HomeScreen> {
     // NOTICE: Manage Advanced Drawer state through the Controller.
     // _advancedDrawerController.value = AdvancedDrawerValue.visible();
     _advancedDrawerController.showDrawer();
+  }
+
+//////////////////////////// Compass /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+  Widget _buildCompass() {
+    final _h = MediaQuery.of(context).size.height;
+    final _w = MediaQuery.of(context).size.width;
+
+    return StreamBuilder<CompassEvent>(
+      stream: FlutterCompass.events,
+      builder: (context, snapshot) {
+        //error msg
+        if (snapshot.hasError) {
+          return Text("ERROR LOADING DATA: ${snapshot.error}");
+        }
+
+        //loading data
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        double? direction = snapshot.data!.heading;
+
+        //if sensor is not working
+        if (direction == null) {
+          return const Center(
+            child: Text("DEVICE DOESNT SUPPORT SENSORS"),
+          );
+        }
+
+        // compass screen
+        return Center(
+          child: Container(
+            height: 800,
+            width: 600,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/blue5.jpg"), fit: BoxFit.cover)),
+            padding: EdgeInsets.all(40),
+            child: Stack(
+              children: [
+                Center(
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(500),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                      child: Container(
+                        height: _h * 0.35,
+                        width: _w * 0.62,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomCenter,
+                              colors: [Colors.white12, Colors.white10]),
+                          borderRadius: BorderRadius.circular(250),
+                          border: Border.all(width: 2, color: Colors.white12),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.all(30),
+                    child: Transform.rotate(
+                        angle: direction * (math.pi / 180) * -1,
+                        child: Image.asset("assets/comp.png")),
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+//////////////////////////// Permission /////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+  Widget _buildPermissionSheet() {
+    final _h = MediaQuery.of(context).size.height;
+    final _w = MediaQuery.of(context).size.width;
+    return Stack(
+      children: [
+        Container(
+          color: Color(0XFFf0f1ec),
+        ),
+        Center(
+          child: Column(
+            children: [
+              SizedBox(
+                height: _h * 0.2,
+              ),
+              Container(
+                height: 230,
+                width: 230,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/comp-logo.png"))),
+              ),
+              Container(
+                height: _h * 0.07,
+                width: _w * 0.6,
+                child: ElevatedButton(
+                    style: ButtonStyle(
+                        backgroundColor:
+                            MaterialStateProperty.all(Color(0xFF21313f)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ))),
+                    onPressed: () {
+                      Permission.locationWhenInUse.request().then((value) {
+                        _fetchPermissionStatus();
+                      });
+                    },
+                    child: const Text("Lets go!")),
+              ),
+            ],
+          ),
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            Container(
+              height: _h * 0.3,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/comp-bottom.png"),
+                      fit: BoxFit.fill)),
+            ),
+          ],
+        )
+      ],
+    );
   }
 }
